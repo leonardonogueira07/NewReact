acb1c8abd77dae8557b0e1888e7218eb54596257
Leonardo Nogueira <leojunio007@gmail.com>
2021-04-28 23:30:55 -0300

    Projeto React - Lista

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
+import './assets/css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://www.mocky.io/v2/5d531c4f2e0000620081ddce`)
+      .then(res => {
+        const users = res.data;
+        this.setState({ users });
+      })
+  }
+
+  render() {
+    return (
+      <>
+        { this.state.users.map(user => 
+            <ul>
+                <li>{user.name}</li>
+                <li>{user.username}</li>
+                <li>{user.img}</li>
+            </ul>     
+        )}
+      </>
+    )
+  }
 }
 
-export default App;
+/*render () (
+  <ul>
+    {Object.keys(this.props.data).map((propKey) => {
+      <li key={propKey}>
+        {this.props.data[propKey]}
+
+        <ul>            
+          {Object.keys(this.props.data[propKey]).map((childPropKey) => {
+            <li key={childPropKey}>
+              {this.props.data[propKey][childPropKey]}
+            </li>
+          })}
+        </ul>
+      </li>
+    })}
+  </ul>
+);*/
+
+
+/*
+class App extends Component {
+
+    state = {
+        users:[]
+    }
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data)
+            this.setState({ data   })
+        })
+        .catch(console.log)
+    }
+    render () {
+        return (
+            <>
+                <p>Olá</p>
+                <ul>
+                    {this.state.users.map(user => <li>{user}</li>)}                 
+                </ul>
+            </>
+            
+        );
+    }
+}
+*/
+//export default App;
